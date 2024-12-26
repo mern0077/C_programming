4c0729b659ed8187f3feba2548f004ff25985038
owner007 <mern0077@gmail.com>
2024-12-26 19:42:01 +0530

    Update index.c

--- index.c
+++ index.c
@@ -301,6 +301,9 @@
 // =========================================================================================================
 
 // question 11
+// it takes three input from user val1,val2 and operator
+// so val1 for numrator operator for specific tast with switch
+// case then val2 for denominator.
 
 // #include <stdio.h>
 
@@ -338,6 +341,7 @@
 // =========================================================================================================
 
 // question 12
+// it take number input from user then sum it using loop
 // #include <stdio.h>
 
 // int main()
@@ -356,24 +360,10 @@
 // =========================================================================================================
 
 // question 13
-// #include <stdio.h>
+// it take position of fibonacci series then its print 
+// data using recursion which replicate itself. it sums 
+// value fibo=fibo(number-1)+fibo(number-2);
 
-// int main()
-// {
-//     int sum = 0, number;
-//     printf("Enter number (it will give you sum at that number) : ");
-//     scanf("%d", &number);
-//     for (int i = 0; i <= number; i++)
-//     {
-//         sum += i;
-//     }
-//     printf("sum at nth number : %d", sum);
-//     return 0;
-// }
-
-// =========================================================================================================
-
-// question 14
 // #include <stdio.h>
 
 // int fibo();
@@ -403,7 +393,10 @@
 
 // =========================================================================================================
 
-// question 15
+// question 14
+// it check prime number from 2 to input value
+// then if input value equal to zero so not prime 
+//else prime.
 // #include <stdio.h>
 
 // int main()
@@ -431,7 +424,9 @@
 
 // =========================================================================================================
 
-// question 16
+// question 15
+// it used object ( key value pair ) when input value gave
+// so his value point that value hence it converted into number.
 // #include <stdio.h>
 // #include <string.h>
 
@@ -506,7 +501,10 @@
 
 // =========================================================================================================
 
-// question 17
+// question 16
+// we create a temp which store end value
+//then end value assined initial value then
+// initial value assigned starting value.
 // #include <stdio.h>
 
 // void reverseArray(int *arr, int len)
@@ -543,7 +541,12 @@
 
 // =========================================================================================================
 
-// question 18
+// question 17
+
+// we are doing traverse so if binary equal to 1
+// so it will power of it then sum it.
+// we are doing LCM so it will convert into binary
+
 // #include <stdio.h>
 // #include <math.h>
 // #include <string.h>
@@ -617,17 +620,205 @@
 
 // =========================================================================================================
 
+// question 18
+// it reverse number using modulus for value reverse for insert
+// n/10 for delete copy value. 
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n, reverse = 0, rem;
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+//     while (n != 0)
+//     {
+//         rem = n % 10;
+//         reverse = reverse * 10 + rem;
+//         n /= 10;
+//     }
+//     printf("Reversed Number: %d", reverse);
+//     return 0;
+// }
+
+// =========================================================================================================
+
 // question 19
+
+//"All Armstrong number between 1 and 1000 are:\n");
+//"yeh to khud se likh yash padh ke"
+
+// #include <math.h>
 // #include <stdio.h>
 
 // int main()
 // {
-//     int arr[8], len = 8;
-//     for (int i = 0; i < len; i++)
+//     int i, sum, num, count = 0;
+//     printf(
+//         "All Armstrong number between 1 and 1000 are:\n");
+
+//     for (i = 1; i <= 100; i++)
 //     {
-//         printf("Enter a decimal number: ");
-//         scanf("%d", &arr[i]);
+//         num = i;
+//         // Count number of digits.
+//         while (num != 0)
+//         {
+//             num /= 10;
+//             count++;
+//         }
+//         num = i;
+//         sum = pow(num % 10, count) + pow((num % 100 - num % 10) / 10, count) + pow((num % 1000 - num % 100) / 100, count);
+
+//         if (sum == i)
+//         {
+//             printf("%d ", i);
+//         }
+//         count = 0;
 //     }
+// }
+
+
+// =========================================================================================================
+
+// question 19
+//long long for manupulate large amount of data. it take binary number
+// and give you decimal value using square of 1 and sum of spuare. 
+
+// #include <stdio.h>
+
+// long long convert(long long);
+
+// int main()
+// {
+
+//     long long n;
+
+//     printf("Enter a binary number: ");
+//     scanf("%lld", &n);
+
+//     printf("%lld in binary = %lld in decimal", n, convert(n));
 
 //     return 0;
 // }
+
+// long long convert(long long n)
+// {
+
+//     long long dec = 0;
+//     int i = 0, rem;
+
+//     while (n != 0)
+//     {
+
+//         rem = n % 10;
+
+//         dec += rem << i;
+
+//         n /= 10;
+
+//         ++i;
+//     }
+
+//     return dec;
+// }
+
+// =========================================================================================================
+
+// question 20
+// it take size of array and we insert data into array then it will do sum using linear search.
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[100], size, i, sum = 0;
+
+//     printf("Enter array size\n");
+//     scanf("%d", &size);
+
+//     printf("Enter array elements\n");
+//     for (i = 0; i < size; i++)
+//         scanf("%d", &arr[i]);
+
+//     for (i = 0; i < size; i++)
+//         sum = sum + arr[i];
+
+//     printf("Sum of the array = %d\n", sum);
+
+//     return 0;
+// }
+
+// =========================================================================================================
+
+// question 21
+//  it take size of two array and we insert data into array then it will do sum using linear search both of them together.
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a[50], b[50], sum[50], i, n;
+
+//     printf("Enter the number of elements in the arrays: ");
+//     scanf("%d", &n);
+
+//     printf("Enter the elements of 1st array: ");
+//     for (i = 0; i < n; i++)
+//         scanf("%d", &a[i]);
+
+//     printf("Enter the elements of 2nd array: ");
+//     for (i = 0; i < n; i++)
+//         scanf("%d", &b[i]);
+
+//     for (i = 0; i < n; i++)
+//         sum[i] = a[i] + b[i];
+
+//     printf("Sum of the corresponding elements of 1st and 2nd arrays: ");
+//     for (i = 0; i < n; i++)
+//         printf("%d ", sum[i]); 
+
+//     return 0;
+// }
+
+// =========================================================================================================
+
+// question 22
+// it take array and first value for min and max value then using for loop it will compare min and max
+// if value is greate it will goes max else it will goes in min
+
+// #include <stdio.h>
+
+// int main() {
+//     int a[50], n, i;
+//     int min, max;
+
+//     // Input the number of elements
+//     printf("Enter the number of elements in the array: ");
+//     scanf("%d", &n);
+
+//     // Input the array elements
+//     printf("Enter the elements of the array: ");
+//     for (i = 0; i < n; i++) {
+//         scanf("%d", &a[i]);
+//     }
+
+//     // Assume the first element is both min and max
+//     min = a[0];
+//     max = a[0];
+
+//     // Loop to find min and max
+//     for (i = 1; i < n; i++) {
+//         if (a[i] < min) {
+//             min = a[i]; // Update min
+//         }
+//         if (a[i] > max) {
+//             max = a[i]; // Update max
+//         }
+//     }
+
+//     // Output the minimum and maximum values
+//     printf("The minimum value in the array is: %d\n", min);
+//     printf("The maximum value in the array is: %d\n", max);
+
+//     return 0;
+// }
+
+
